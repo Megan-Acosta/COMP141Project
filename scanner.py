5fd20ff8bad784dfa098e4b4e50a18846934a0ee
Megan Acosta <92880180+Megan-Acosta@users.noreply.github.com>
2024-02-08 21:10:02 -0800

    Create scanner.py
    first commit

---
+++ scanner.py
@@ -0,0 +1,27 @@
+"""
+Author: Megan Acosta, Diego Arroyo
+Course: COMP 141
+Assignment: Project Phase 1 - Scanner 1.1
+"""
+#!/usr/bin/python3
+
+class Token:
+
+    token_type = " "
+    token_value = " " 
+  
+def readFile():
+    """
+    This function will open and read from the input file
+    """
+
+def main():
+    """
+    This is the main entry function
+    """
+    filename = input("Enter the name of the file: ")
+    print(filename)
+
+if __name__ == "__main__":
+    main()
+  
